5fd98353086f7e7a118b74cd060942ed2879dd55
imposibrus <imposibrus@gmail.com>
2016-02-08 18:27:14 +0300

    fix - custom views in `List` - support for absolute paths

--- controllers/listCollection.js
+++ controllers/listCollection.js
@@ -13,11 +13,21 @@ module.exports = function(options) {
       return Promise.resolve(collection).map(function(document) {
         return populateItem(document, modelConfig, options.models);
       }).then(function(populatedDocuments) {
-        var templateName = 'list';
+        var templateName = 'list',
+            templatePath = '';
+
         if(modelConfig.templates && modelConfig.templates.list) {
           templateName = path.basename(modelConfig.templates.list, path.extname(modelConfig.templates.list));
+          if(path.isAbsolute(modelConfig.templates.list)) {
+            templatePath = modelConfig.templates.list;
+          } else {
+            templatePath = path.join(viewsFolder, 'admin/' + templateName);
+          }
+        } else {
+          templatePath = path.join(viewsFolder, 'admin/' + templateName);
         }
-        res.render(path.join(viewsFolder, 'admin/' + templateName), {
+
+        res.render(templatePath, {
           modelConfig: modelConfig,
           adminConfig: options.adminConfig,
           collection: populatedDocuments
