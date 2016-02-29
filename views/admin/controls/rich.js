20d820d87e5a10971aff6e865961b157631eb668
imposibrus <imposibrus@gmail.com>
2016-02-29 18:32:38 +0300

    fix - textarea - move nl2br to rich controller

--- views/admin/controls/rich.js
+++ views/admin/controls/rich.js
@@ -1,12 +1,15 @@
 
 var path = require('path'),
-    _ = require('lodash');
+    _ = require('lodash'),
+    humanize = require('humanize');
 
 module.exports = function(res, field, document, cb) {
   var classesArray = (field.class || '').split(' ');
   if(classesArray.indexOf('tinymce') == -1) {
     classesArray.push('tinymce');
   }
+
+  field.value = humanize.nl2br(field.value);
 
   field.class = _.uniq(_.compact(classesArray)).join(' ');
   res.render(path.join(__dirname, 'textarea'), {field: field, document: document}, cb);
